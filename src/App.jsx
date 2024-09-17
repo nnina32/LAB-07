7c430aa13febf02aab33fac0e659e305e0cdc3ef
nnina32 <noeminina18@gmail.com>
2024-09-17 02:13:58 -0400

    lab-07

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { WapperList } from './componentes/WrapperList';
+import { List } from './componentes/List'
+
+function App() {
+
+  return (
+    <>
+      <WapperList>
+        <List/>
+      </WapperList>      
+    </>
+  )
+}
+
+export default App
